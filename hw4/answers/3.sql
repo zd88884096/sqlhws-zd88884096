a3621c1dd269559ec0c94b84d2cb3ebd9f757a07
Duo Zheng <duoz@vdesk2.cs.uchicago.edu>
2022-02-12 20:52:52 -0600

    second

--- hw4/answers/3.sql
+++ hw4/answers/3.sql
@@ -1,7 +1,5 @@
-select sum(case when a.sums > 0 then 1 else 0 end) as count
-from
-(select sum(case when r.business_id is null then 0 else 1 end) as sums
+select count(1) as count
 from business as b
 left join review as r
 on b.business_id = r.business_id
-group by b.business_id) as a
+where r.business_id is null;
